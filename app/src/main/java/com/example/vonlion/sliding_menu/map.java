3fc9e44ddc4e4ed17d16abd52e484ef7960dd153
wythe_wee@outlook.com <wythe_wee@outlook.com>
2016-02-01 21:28:39 +0800

    添加计时功能，和路程计数功能

--- app/src/main/java/com/example/vonlion/sliding_menu/map.java
+++ app/src/main/java/com/example/vonlion/sliding_menu/map.java
@@ -13,12 +13,16 @@ import android.os.Handler;
 import android.os.Message;
 import android.os.SystemClock;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.amap.api.location.AMapLocation;
 import com.amap.api.location.AMapLocationListener;
 import com.amap.api.location.LocationManagerProxy;
 import com.amap.api.location.LocationProviderProxy;
 import com.amap.api.maps.AMap;
+import com.amap.api.maps.AMapUtils;
 import com.amap.api.maps.CameraUpdate;
 import com.amap.api.maps.CameraUpdateFactory;
 import com.amap.api.maps.LocationSource;
@@ -27,7 +31,11 @@ import com.amap.api.maps.model.ArcOptions;
 import com.amap.api.maps.model.CameraPosition;
 import com.amap.api.maps.model.LatLng;
 
-public class map extends Activity implements LocationSource, AMapLocationListener{
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class map extends Activity implements LocationSource,
+        View.OnClickListener,AMapLocationListener{
     private MapView mapView;
     private AMap aMap;
     private OnLocationChangedListener mListener;
@@ -35,7 +43,14 @@ public class map extends Activity implements LocationSource, AMapLocationListene
     private Intent alarmIntent = null;
     private PendingIntent alarmPi = null;
     private AlarmManager alarm = null;
-    LatLng [] latLngs = new LatLng[3];
+    private LatLng [] latLngs = new LatLng[3];
+    private TextView tvDistance = null;
+    private TextView tvShowTime;
+    private Button btStart;
+    private int totalSec = 0;
+    private Timer timer = null;
+    private Message msg = null;
+    private TimerTask task = null;
     int cnt = 0;
 
 
@@ -44,8 +59,13 @@ public class map extends Activity implements LocationSource, AMapLocationListene
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
 
+        tvDistance = (TextView) findViewById(R.id.tvDistance);
+        tvShowTime = (TextView) findViewById(R.id.tvTime);
+        btStart = (Button) findViewById(R.id.btStart);
         mapView = (MapView) findViewById(R.id.map);
         mapView.onCreate(savedInstanceState);
+
+        btStart.setOnClickListener(this);
         init();
     }
 
@@ -76,33 +96,52 @@ public class map extends Activity implements LocationSource, AMapLocationListene
     Handler mHandler = new Handler() {
         public void dispatchMessage(android.os.Message msg) {
             AMapLocation loc = (AMapLocation) msg.obj;
-
-            //画轨迹曲线
-            if(loc.getAccuracy() < 10f){
-                latLngs[cnt] = new LatLng(loc.getLatitude(),loc.getLongitude());
-            }
-            if(cnt == 2){
-                drawArc(latLngs);
-                latLngs[0]=latLngs[2];
-                cnt = 0;
-//                Toast.makeText(getApplicationContext(),"draw", Toast.LENGTH_SHORT).show();
-            }
-            cnt++;
-
             // 显示系统小蓝点
             mListener.onLocationChanged(loc);
-
-            //视图相机移动到当前位置
-            CameraPosition cameraPosition;
-            CameraUpdate cameraUpadate;
-            cameraPosition = new CameraPosition(new LatLng(loc.getLatitude(), loc.getLongitude()), 18.0f, 0.0f, 0.0f);
-            cameraUpadate = CameraUpdateFactory.newCameraPosition(cameraPosition);
-            aMap.animateCamera(cameraUpadate);
-
-            //获取当前屏幕中心点 位置信息
-            cameraPosition = aMap.getCameraPosition();
+            displayDistance(length);
+            drawTrace(loc);
+            camMoveToCurPos(loc);
         }
     };
+    //显示跑步时间
+    public void displayTime(){
+
+    }
+
+    //显示跑步距离
+    public void displayDistance(double length){
+        if(length < 1000){
+            tvDistance.setText((int)length+"m");
+        }
+        else if(length >= 1000){
+            int firstPart = (int)length/1000;
+            int secondPart = ((int)length%1000)/100;
+            tvDistance.setText(firstPart+"."+secondPart+"km");
+        }
+    }
+
+    //画轨迹
+    public void drawTrace(AMapLocation loc) {
+         //需要一个全局计数器cnt
+        if (loc.getAccuracy() < 10f) {
+            latLngs[cnt] = new LatLng(loc.getLatitude(), loc.getLongitude());
+        }
+        if (cnt == 2) {
+            drawArc(latLngs);
+            latLngs[0] = latLngs[2];
+            cnt = 0;
+        }
+        cnt++;
+    }
+
+    //视图相机移动到当前位置
+    public void camMoveToCurPos(AMapLocation loc){
+        CameraPosition cameraPosition;
+        CameraUpdate cameraUpadate;
+        cameraPosition = new CameraPosition(new LatLng(loc.getLatitude(), loc.getLongitude()), 18.0f, 0.0f, 0.0f);
+        cameraUpadate = CameraUpdateFactory.newCameraPosition(cameraPosition);
+        aMap.animateCamera(cameraUpadate);
+    }
 
     /**
      * 初始化AMap对象
@@ -160,7 +199,7 @@ public class map extends Activity implements LocationSource, AMapLocationListene
                 //在定位结束后，在合适的生命周期调用destroy()方法
                 //其中如果间隔时间为-1，则定位只定一次
                 locationClient.requestLocationData(
-                        LocationProviderProxy.AMapNetwork, 1000, 3,map.this);
+                        LocationProviderProxy.AMapNetwork, 1000, 3, map.this);
             }
         }
     };
@@ -198,6 +237,9 @@ public class map extends Activity implements LocationSource, AMapLocationListene
      * 定位成功后回调函数
      * 位置发生变化后，向Handler发送loc位置信息对象
      */
+    int flag = 0;
+    LatLng [] las = new LatLng[2];
+    double length = 0;
     @Override
     public void onLocationChanged(AMapLocation loc) {
         if(null != loc){
@@ -205,6 +247,14 @@ public class map extends Activity implements LocationSource, AMapLocationListene
             msg.obj = loc;
             mListener.onLocationChanged(loc);
             mHandler.sendMessage(msg);
+            if(loc.getAccuracy() < 30f){
+                las[flag] = new LatLng(loc.getLatitude(),loc.getLongitude());
+                if(flag == 1){
+                    length+= AMapUtils.calculateLineDistance(las[0],las[1]);
+                    flag = -1;
+                }
+                flag++;
+            }
         }
     }
 
@@ -233,4 +283,58 @@ public class map extends Activity implements LocationSource, AMapLocationListene
 
     @Override
     public void onProviderDisabled(String provider) {}
+
+    Handler timerHandler = new Handler(){
+        @Override
+        public void handleMessage(Message msg){
+            totalSec++;
+
+            int min = totalSec/60;
+            int sec = totalSec%60;
+            int hour = min/60;
+            min = min%60;
+            tvShowTime.setText(String.format(
+                    "%1$02d:%2$02d:%3$02d",hour,min,sec
+            ));
+
+           super.handleMessage(msg);
+        }
+    };
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId() == R.id.btStart){
+            if(btStart.getText().equals("开始")){
+                Toast.makeText(getApplicationContext(),"开始",Toast.LENGTH_SHORT).show();
+                btStart.setText("暂停");
+                if (null == timer) {
+                    if (null == task) {
+                        task = new TimerTask() {
+                            @Override
+                            public void run() {
+                                // TODO Auto-generated method stub
+                                if (null == msg) {
+                                    msg = new Message();
+                                } else {
+                                    msg = Message.obtain();
+                                }
+                                timerHandler.sendMessage(msg);
+                            }
+                        };
+                    }
+                    timer = new Timer(true);
+                    timer.schedule(task, 1000, 1000);
+                }
+            }
+            else if(btStart.getText().equals("暂停")){
+                Toast.makeText(getApplicationContext(),"暂停",Toast.LENGTH_SHORT).show();
+                btStart.setText("开始");
+                task.cancel();
+                task = null;
+                timer.cancel();
+                timer.purge();
+                timer = null;
+            }
+        }
+    }
 }
