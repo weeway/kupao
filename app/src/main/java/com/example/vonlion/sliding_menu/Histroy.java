b640c2c474b49434a6753f6f1630a07806e39a4b
wythe_wee@outlook.com <wythe_wee@outlook.com>
2016-02-20 23:12:05 +0800

    小问题修复

--- app/src/main/java/com/example/vonlion/sliding_menu/Histroy.java
+++ app/src/main/java/com/example/vonlion/sliding_menu/Histroy.java
@@ -14,7 +14,6 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
-import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -31,42 +30,44 @@ public class Histroy  extends Activity implements AdapterView.OnItemLongClickLis
     String USER_NAME;
     ActionMode mActionMode = null;
     String startTimeFlag = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.histroy);
 
-        DatabaseHelper database = new DatabaseHelper(this);
-        SQLiteDatabase db = database.getReadableDatabase();
-        SharedPreferences share = getSharedPreferences("User_date",map.MODE_PRIVATE);
-        USER_NAME =share.getString("username", "");
-        if(USER_NAME!=null) {
-            cursor = db.query("usertb", null, "name like?", new String[]{USER_NAME}, null, null, "name");
-//            Toast.makeText(Histroy.this.getApplicationContext(), "USER_NAME不为空", Toast.LENGTH_SHORT).show();
-        }
-        else {
-//            Toast.makeText(Histroy.this.getApplicationContext(), "USER_NAME为空", Toast.LENGTH_SHORT).show();
-        }
-        ListView list = (ListView) findViewById(R.id.ListView01);
-        SimpleAdapter adapter = new SimpleAdapter(this, getData(), R.layout.historyitem,
-                new String[]{"date", "img1", "state", "distance", "img2", "time", "img3", "step"},
-                new int[]{R.id.date, R.id.img1, R.id.state, R.id.distance, R.id.img2, R.id.time, R.id.img3, R.id.step});
-        list.setAdapter(adapter);
+//        DatabaseHelper database = new DatabaseHelper(this);
+//        SQLiteDatabase db = database.getReadableDatabase();
+//        SharedPreferences share = getSharedPreferences("User_date",map.MODE_PRIVATE);
+//        USER_NAME =share.getString("username", "");
+//        if(USER_NAME!=null) {
+//            cursor = db.query("usertb", null, "name like?", new String[]{USER_NAME}, null, null, "name");
+////            Toast.makeText(Histroy.this.getApplicationContext(), "USER_NAME不为空", Toast.LENGTH_SHORT).show();
+//        }
+//        else {
+////            Toast.makeText(Histroy.this.getApplicationContext(), "USER_NAME为空", Toast.LENGTH_SHORT).show();
+//        }
+//        ListView list = (ListView) findViewById(R.id.ListView01);
+//        SimpleAdapter adapter = new SimpleAdapter(this, getData(), R.layout.historyitem,
+//                new String[]{"date", "img1", "state", "distance", "img2", "time", "img3", "step"},
+//                new int[]{R.id.date, R.id.img1, R.id.state, R.id.distance, R.id.img2, R.id.time, R.id.img3, R.id.step});
+//        list.setAdapter(adapter);
+//
+//        //设置跳转到历史主界面
+//        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            public void onItemClick(AdapterView<?> parent, View view,
+//                                    int position, long id) {
+//                ListView listView = (ListView) parent;
+//                HashMap<String, String> map1 = (HashMap<String, String>) listView.getItemAtPosition(position);
+//                DATE = map1.get("date");
+//                Intent intent = new Intent(Histroy.this, Histroy_chart.class);
+//                intent.putExtra("date",DATE);
+//                startActivity(intent);
+//            }
+//        });
 
-        //设置跳转到历史主界面
-        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            public void onItemClick(AdapterView<?> parent, View view,
-                                    int position, long id) {
-                ListView listView = (ListView) parent;
-                HashMap<String, String> map1 = (HashMap<String, String>) listView.getItemAtPosition(position);
-                DATE = map1.get("date");
-                Intent intent = new Intent(Histroy.this, Histroy_chart.class);
-                intent.putExtra("date",DATE);
-                startActivity(intent);
-            }
-        });
-
-        list.setOnItemLongClickListener(this);
+//        list.setOnItemLongClickListener(this);
+        refreshListView();
     }
 
     @Override
@@ -74,19 +75,13 @@ public class Histroy  extends Activity implements AdapterView.OnItemLongClickLis
         super.onResume();
 
     }
-    public void showListView(){
+    public void refreshListView() {
         DatabaseHelper database = new DatabaseHelper(this);
         SQLiteDatabase db = database.getReadableDatabase();
-        SharedPreferences share = getSharedPreferences("User_date",map.MODE_PRIVATE);
-        USER_NAME =share.getString("username", "");
-        if(USER_NAME!=null) {
-            cursor = db.query("usertb", null, "name like?", new String[]{USER_NAME}, null, null, "name");
-            Toast.makeText(Histroy.this.getApplicationContext(), "USER_NAME不为空", Toast.LENGTH_SHORT).show();
-        }
-        else {
-            Toast.makeText(Histroy.this.getApplicationContext(), "USER_NAME为空", Toast.LENGTH_SHORT).show();
-        }
-        ListView list = (ListView) findViewById(R.id.ListView01);
+        SharedPreferences share = getSharedPreferences("User_date", map.MODE_PRIVATE);
+        USER_NAME = share.getString("username", "");
+        cursor = db.query("usertb", null, "name like?", new String[]{USER_NAME}, null, null, "name");
+        final ListView list = (ListView) findViewById(R.id.ListView01);
         SimpleAdapter adapter = new SimpleAdapter(this, getData(), R.layout.historyitem,
                 new String[]{"date", "img1", "state", "distance", "img2", "time", "img3", "step"},
                 new int[]{R.id.date, R.id.img1, R.id.state, R.id.distance, R.id.img2, R.id.time, R.id.img3, R.id.step});
@@ -100,17 +95,14 @@ public class Histroy  extends Activity implements AdapterView.OnItemLongClickLis
                 HashMap<String, String> map1 = (HashMap<String, String>) listView.getItemAtPosition(position);
                 DATE = map1.get("date");
                 Intent intent = new Intent(Histroy.this, Histroy_chart.class);
-                intent.putExtra("date",DATE);
+                intent.putExtra("date", DATE);
                 startActivity(intent);
             }
         });
 
         list.setOnItemLongClickListener(this);
     }
-    /**
-     * 获取数据
-     * @return
-     */
+    
     private List<Map<String,Object>> getData() {
 
         List<Map<String, Object>> list1 = new ArrayList<Map<String, Object>>();
@@ -191,7 +183,8 @@ public class Histroy  extends Activity implements AdapterView.OnItemLongClickLis
             case R.id.delete:
 //                Toast.makeText(getApplicationContext(),"dianji",Toast.LENGTH_LONG).show();
                 deleteItem();
-                showListView();
+                refreshListView();
+                mode.finish();
                 return true;
             default:
                 return false;
